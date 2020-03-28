30a5f28e2dbb425743d86f7fd0aa71259261ef14
Hugo Burbelo <hburbelo@phone2action.com>
2020-03-28 13:25:34 -0400

    Cleanup formatting of backend function

--- airtable-backend/index.js
+++ airtable-backend/index.js
@@ -8,33 +8,25 @@ exports.register = function(event, context, callback) {
   const airtableBase = body.base;
   const airtableTable = body.table;
   const airtableColumn = body.column;
-  console.log(airtableAPIKey)
-  var base = new Airtable({apiKey: 'key0C4CA8xtuHMDDN'}).base('app4raQKdmoN34zFV');
+
+  const base = new Airtable({apiKey: 'key0C4CA8xtuHMDDN'}).base('app4raQKdmoN34zFV');
 
   let entry = {
-    "fields": {
-
-    }
+    "fields": {}
   }
 
   entry.fields[airtableColumn] = email
 
-  base('Emails')
-    .create([entry],function done(err) {
-            if (err) {
-              callback(err)
-            } else {
-              const body = JSON.stringify({ records: airtableColumn })
-              const response = {
-                statusCode: 200,
-                body: body,
-                headers: {
-                  'content-type': 'application/json',
-                  'cache-control': 'Cache-Control: max-age=300, public'
-                }
-              }
-              callback(null, response)
-            }
-          }
-  )
+  base('Emails').create([entry], function done(err) {
+    if (err) {
+      callback(err)
+    } else {
+      const body =
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ records: email })
+      }
+      callback(null, response)
+    }
+  });
 }
